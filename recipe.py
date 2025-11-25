53a5846acc91dcdf99790cfb71cf13fb2831d025
TeoGiane <gianeteo@hotmail.it>
2025-11-25 18:20:10 +0100

    Using main recipe.py for all subfolders as extra modules

--- recipe.py
+++ recipe.py
@@ -1,4 +1,15 @@
-from cook import create_task
+import sys
+import string
+from cook import create_task, Task
+from cook.contexts import FunctionContext
+
+# Modify task name according to subfolder
+def append_name(task: Task, name: str) -> Task:
+    task.name = f"_{name}:{task.name}" if task.name.startswith("_") else f"{name}:{task.name}"
+    return task
+
+# Include the current directory for extra modules
+sys.path.append(".")
 
 # Create requirements task
 create_task("requirements", action="pip-compile -v", targets=["requirements.txt"],
@@ -6,3 +17,7 @@ create_task("requirements", action="pip-compile -v", targets=["requirements.txt"
 
 # Create pip-sync task
 create_task("pip-sync", action="pip-sync", dependencies=["requirements.txt"])
+
+# Include tasks from 'poisson_model_lattice' subfolder
+with FunctionContext(append_name, "poisson_model_lattice"):
+    from poisson_model_lattice.recipe import *
