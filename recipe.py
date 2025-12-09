748a52e83d2c78a7c1a6a9f925c414c971627882
TeoGiane <gianeteo@hotmail.it>
2025-12-09 14:50:37 +0100

    Update recipe.py

--- recipe.py
+++ recipe.py
@@ -1,15 +1,17 @@
+import os
 import sys
-import string
+
 from cook import create_task, Task
 from cook.contexts import FunctionContext
 
+# Include the current directory for extra modules
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 # Modify task name according to subfolder
 def append_name(task: Task, name: str) -> Task:
-    task.name = f"_{name}:{task.name}" if task.name.startswith("_") else f"{name}:{task.name}"
+    root_name = task.name.split(':', 1)[1] if ":" in task.name else task.name
+    task.name = f"_{name}:{root_name}" if task.name.startswith("_") else f"{name}:{root_name}"
     return task
-
-# Include the current directory for extra modules
-sys.path.append(".")
 
 # Create requirements task
 create_task("requirements", action="pip-compile -v", targets=["requirements.txt"],
@@ -21,3 +23,7 @@ create_task("pip-sync", action="pip-sync", dependencies=["requirements.txt"])
 # Include tasks from 'poisson_model_lattice' subfolder
 with FunctionContext(append_name, "poisson_model_lattice"):
     from poisson_model_lattice.recipe import *
+
+# include tasks from 'covid_data' subfolder
+with FunctionContext(append_name, "covid_data"):
+    from covid_data.recipe import *
